cb65a142e87b7043af2dc9ffbddc5d0e3ac50609
Volodymyr Zhdanov <wight554@gmail.com>
2022-03-27 20:48:01 +0300

    vite-plugin-typescript: add missing export
    * derp

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,5 @@ import { RollupTypescriptOptions } from '@rollup/plugin-typescript';
 import type { Plugin } from 'vite';
 
 declare function typescript(options?: RollupTypescriptOptions): Plugin;
+
+export default typescript;
